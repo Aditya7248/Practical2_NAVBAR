caaa1d30d13eecc7bf57db42aa70408045ff1ddc
Aditya Tiwari <tiwariaditya2707@gmail.com>
2023-07-19 09:27:02 +0530

    Navbar using materialUI

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+
+const Navbar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        
+          <img src={require('./mylogo.png')} alt="Logo" style={{ height: '45px', marginRight: '10px'}} />
+        </Typography>
+        <Button color="inherit">Home</Button>
+        <Button color="inherit">About</Button>
+        <Button color="inherit">Services</Button>
+        <Button color="inherit">Contact</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
